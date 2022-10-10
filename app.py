4d2ba1ba31493f038a3f8c77d4c07bc90b85e903
Akhila574746 <87166392+Akhila574746@users.noreply.github.com>
2022-10-10 13:30:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title('Fake Job Detection') #creates a title in web app
 ip = st.text_input('Enter Job Description:') #creates a text box in web app
 vect = CountVectorizer()
 ip_text = vect.transform([ip])
-op = model_nb.predict([ip_text])
+op = model_nb.predict(ip_text)
 if st.button('Predict'):
   st.title(op[0])
   if (op[0]==1):
