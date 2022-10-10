229830d6e6cc6a7527b12e6af15b994dcd47bca0
Akhila574746 <87166392+Akhila574746@users.noreply.github.com>
2022-10-10 12:31:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-model_nb = joblib.load('Fake_Job_Detection')
+model_nb = joblib.load('Fake_job_postings')
 st.title('Fake Job Detection') #creates a title in web app
 ip = st.text_input('Enter Job Description:') #creates a text box in web app
 op = model_nb.predict([ip])
