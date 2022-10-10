c6d5271b57bf0eb09e48baa0f7ae182d62fd81ac
Akhila574746 <87166392+Akhila574746@users.noreply.github.com>
2022-10-10 12:51:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import streamlit as st
 import joblib
+from sklearn.feature_extraction.text import CountVectorizer
 model_nb = joblib.load('Fake_job_postings')
 st.title('Fake Job Detection') #creates a title in web app
 ip = st.text_input('Enter Job Description:') #creates a text box in web app
-op = model_nb.predict([ip])
+vect = CountVectorizer()
+ip_text = vect.transform(ip)
+op = model_nb.predict([ip_text])
 if st.button('Predict'):
   st.title(op[0])
+  if (op[0]==1):
+    print('Fraudulant Job')
+  else:
+    print('Real Job')
   
   
   
   
   
-  
-  
+ 
+
