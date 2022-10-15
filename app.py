94c47a30333d05081e6ec25f63607d90192fbfed
Akhila574746 <87166392+Akhila574746@users.noreply.github.com>
2022-10-15 15:43:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import streamlit as st
-import joblib
-from sklearn.feature_extraction.text import CountVectorizer
-model_nb = joblib.load('Fake_job_postings')
-st.title('Fake Job Detection') #creates a title in web app
-ip = st.text_input('Enter Job Description:') #creates a text box in web app
-vect = CountVectorizer()
-ip_text = vect.transform(ip).toarray()
-op = model_nb.predict([ip_text])
-if st.button('Predict'):
-  st.title(op[0])
-  if (op[0]==1):
-    print('Fraudulant Job')
-  else:
-    print('Real Job')
-  
+import pickle
+
+model_nb = pickle.load('Fake_job_postings')
+cv = pickle.load(open("vectorizer","rb"))
+
+def main:
+  st.title('Fake Job Detection') #creates a title in web app
+  ip = st.text_input('Enter Job Description:') #creates a text box in web app
+  if st.button('Predict'):
+    data=[ip]
+    vect=cv.transform(data).toarray()
+    prediction=model_nb.predict(vect)
+    result=prediction[0]
+    if result==0:
+      st.error("Fraudulant Job")
+    else:
+      st.success("Real Job")
+   
+main()  
   
   
   
