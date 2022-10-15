6497fde04a1131f2589aa07fb25971d1afad51ad
Akhila574746 <87166392+Akhila574746@users.noreply.github.com>
2022-10-15 16:06:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ import pickle
 import joblib
 
 model_nb = joblib.load('Fake_job_postings')
-cv = pickle.load(open("vectorizer","rb"))
+vect = pickle.load(open("vectorizer","rb"))
 
 def main():
   st.title('Fake Job Detection') #creates a title in web app
   ip = st.text_input('Enter Job Description:') #creates a text box in web app
   if st.button('Predict'):
     data=[ip]
-    vect=cv.transform(data).toarray()
-    prediction=model_nb.predict(vect)
+    cv=vect.transform(data).toarray()
+    prediction=model_nb.predict(cv)
     result=prediction[0]
     if result==0:
       st.error("Fraudulant Job")
